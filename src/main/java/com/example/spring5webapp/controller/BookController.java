e94b6c9adb327f1c3b12a8d852b555244975dff3
Saul Recinos <erecinos@hightech-corp.com>
2021-10-07 18:28:34 -0600

    Templates and new Entity added

---
+++ src/main/java/com/example/spring5webapp/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.example.spring5webapp.controller;
+
+import com.example.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepo;
+
+    public BookController(BookRepository bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepo.findAll());
+        return "bookView";
+    }
+}
